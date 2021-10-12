de3c4f7be75824c3bd85a0e9b47e2efd703563fe
spacedude609 <jwmidds@outlook.com>
2021-10-12 12:38:41 -0400

    Add something to change console color

--- ccp_text.py
+++ ccp_text.py
@@ -1,14 +1,18 @@
 from time import sleep
 from sys import exit
+from os import system
 def e(message):
     """Quits the program with a specific message"""
     exit([message])
-
+system("color 4E")
 print("Welcome to the CCP Social credit test!")
 sleep(2)
+print("You need to pass this test in order to immigrate to china!")
 name = input("Please enter your name: ")
 if name == "John Xina":
     print("You cannot take this test!")
     sleep(1)
     print("You downloaded illegal social credit points!")
     e("ERR_ILLEGAL_IMMIGRANT")
+system("color")
+
