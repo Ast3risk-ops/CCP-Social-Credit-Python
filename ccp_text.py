a4f7dfed9326e54fe793367b646297a0bc6ee162
spacedude609 <jwmidds@outlook.com>
2021-10-12 12:32:11 -0400

    Get started with development. Made the first function!

--- ccp_text.py
+++ ccp_text.py
@@ -1,4 +1,14 @@
 from time import sleep
+from sys import exit
+def e(message):
+    """Quits the program with a specific message"""
+    exit([message])
+
 print("Welcome to the CCP Social credit test!")
 sleep(2)
-name = input("Please enter you name: ")
+name = input("Please enter your name: ")
+if name == "John Xina":
+    print("You cannot take this test!")
+    sleep(1)
+    print("You downloaded illegal social credit points!")
+    e("ERR_ILLEGAL_IMMIGRANT")
