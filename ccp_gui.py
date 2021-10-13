0b3a4765bee954c840661e06525c85df4f052d94
spacedude609 <jwmidds@outlook.com>
2021-10-13 12:29:07 -0400

    Make first question

--- ccp_gui.py
+++ ccp_gui.py
@@ -1,4 +1,6 @@
-from guizero import App
+from guizero import App, Text
 from sys import exit
 app = App(title="社會信用測試", bg="red", layout="grid")
+q1 = Text(app, text="Question 1", color="yellow", grid=[100,5])
+
 app.display()
