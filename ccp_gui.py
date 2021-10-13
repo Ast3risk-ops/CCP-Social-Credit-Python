e8812bbf17d48d7b8eb6137719c946483ed58ff8
spacedude609 <jwmidds@outlook.com>
2021-10-13 12:42:28 -0400

    Finish text of first question

--- ccp_gui.py
+++ ccp_gui.py
@@ -1,6 +1,7 @@
 from guizero import App, Text, ButtonGroup
 from sys import exit
-app = App(title="社會信用測試", bg="red", layout="grid")
-q1 = Text(app, text="Question 1", color="yellow", grid=[100,5])
-
+app = App(title="社會信用測試", bg="red", layout="auto")
+q1 = Text(app, text="Question 1", color="yellow")
+q1_text= Text(app, text="How many children do you have?")
+q1_choice = ButtonGroup(app, options=[["1", "1"],  ["2", "2"], ["More than 2", "3"]])
 app.display()
