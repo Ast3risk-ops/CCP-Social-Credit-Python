a27c544ef34634d6301fd2cc3d7edf07fcc0fd9a
spacedude609 <jwmidds@outlook.com>
2021-10-13 12:35:09 -0400

    Add import statement

--- ccp_gui.py
+++ ccp_gui.py
@@ -1,4 +1,4 @@
-from guizero import App, Text
+from guizero import App, Text, ButtonGroup
 from sys import exit
 app = App(title="社會信用測試", bg="red", layout="grid")
 q1 = Text(app, text="Question 1", color="yellow", grid=[100,5])
