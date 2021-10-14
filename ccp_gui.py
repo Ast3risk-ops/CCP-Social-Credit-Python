dda64f04da5b3d3811a0b00e70a0e84f924e83cb
spacedude609 <jwmidds@outlook.com>
2021-10-14 18:47:35 -0400

    reformat file properly

--- ccp_gui.py
+++ ccp_gui.py
@@ -1,13 +1,18 @@
 from guizero import App, Text, ButtonGroup
 from sys import exit
+
+
 def e(message):
     app.destroy()
     exit([message])
+
+
 app = App(title="社會信用測試", bg="red", layout="auto")
+
 app.set_full_screen("}")
 app.info("社會信用測試開始", "Welcome to the Social Credit Test!")
 app.warn("警告！", "You will not be able to use your pc until you finish the test!")
 q1 = Text(app, text="Question 1", color="yellow")
 q1_text = Text(app, text="How many children do you have?")
-q1_choice = ButtonGroup(app, options=[["1", "1"],  ["2", "2"], ["More than 2", "3"]])
+q1_choice = ButtonGroup(app, options=[["1", "1"], ["2", "2"], ["More than 2", "3"]])
 app.display()
